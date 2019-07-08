728a69a8d14a4b2b874284471eba0bf83ed3ea54
ooobenooo <benhuang81@gmail.com>
2019-07-08 09:29:07 +0000

    session manager

--- src/session/manager.go
+++ src/session/manager.go
@@ -10,3 +10,24 @@ type Manager struct {
 	provider    Provider   // Session可以存储在任意计算机的存储介质，生命周期抽象管理接口
 	maxLifeTime int64
 }
+
+var providers = make(map[string]Provider)
+
+// 注册Provider
+func RegisterProvider(name string, provider Provider) {
+    if _, exist := providers[name]; exist {
+        panic(name, "is exists. do not allow register again.")
+    }
+    providers[name] = provider
+}
+
+// 创建新的Session Manager，调用方维护一个Manager的全局变量实例
+func NewManager(providerName string, cookieName string, maxLifeTime int64) (*Manager, error) {
+    provider, ok := providers[providerName]
+    if !ok {
+        return nil, fmt.Errorf("session: unknown provider %q", providerName)
+    }
+    
+    return &Manager{cookieName: cookieName, provider: provider, maxLifeTime: maxLifeTime}, nil
+}
+
