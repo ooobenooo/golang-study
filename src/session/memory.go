3e461052b2cfef18136190627bd0eb9b3cc055f9
ooobenooo <benhuang81@gmail.com>
2019-07-09 09:59:09 +0000

    session init

--- src/session/memory.go
+++ src/session/memory.go
@@ -6,9 +6,12 @@ import (
     "time"
 )
 
+// 创建一个内存Session存储实例
+var memoryProvider = &MemoryProvider{list: list.New()}
+
 // 实现Session接口，被Web交互接口调用
 type SessionStore struct {
-    sid string,
+    sid string
     timeAccessed time.Time
     value map[interface{}]interface{}
 }
@@ -19,4 +22,17 @@ type MemoryProvider struct {
     lock sync.Mutex
     sessions map[string]*list.Element //存session
     list *list.List //GC清理
+}
+
+// 初始化session实例
+// 存储到内存
+func (p *MemoryProvider) SessionInit(sid string) (Session, error) {
+    p.lock.Lock()
+    defer p.lock.Unlock()
+    
+    v := make(map[interface{}]interface{}, 0)
+    session := &SessionStore{sid, time.Now(), v}
+    element := p.list.PushBack(session)
+    p.sessions[sid] = element
+    return session, nil
 }
