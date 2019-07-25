71f6cbefd14da7a9b710d5443ca1f473c00b71ea
ooobenooo <benhuang81@gmail.com>
2019-07-26 07:12:02 +0800

    websocket

--- src/udp_simple_server/main.go
+++ src/udp_simple_server/main.go
@@ -1,37 +1,37 @@
 package main
 
 import (
-    "fmt"
-    "net"
-    "os"
-    "time"
+	"fmt"
+	"net"
+	"os"
+	"time"
 )
 
 func main() {
-    service := ":28080"
-    udpAddr, err := net.ResolveUDPAddr("udp", service)
-    checkError(err)
-    conn, err := net.ListenUDP("udp", udpAddr)
-    checkError(err)
-    for {
-        handleConn(conn) // GO的UDP没有accept， 一个conn共享，不要主动关闭conn
-    }
+	service := ":28080"
+	udpAddr, err := net.ResolveUDPAddr("udp", service)
+	checkError(err)
+	conn, err := net.ListenUDP("udp", udpAddr)
+	checkError(err)
+	for {
+		handleConn(conn) // GO的UDP没有accept， 一个conn共享，不要主动关闭conn
+	}
 }
 
-func handleConn(conn net.Conn) {
-    var buf [512]byte
-    
-    _, addr, err := conn.ReadFromUDP(buf[0:])
-    if err != nil {
-        return
-    }
-    dayTime := time.Now().String()
-    conn.WriteToUDP([]byte(dayTime), addr)
+func handleConn(conn *net.UDPConn) {
+	var buf [512]byte
+
+	_, addr, err := conn.ReadFromUDP(buf[0:])
+	if err != nil {
+		return
+	}
+	dayTime := time.Now().String()
+	conn.WriteToUDP([]byte(dayTime), addr)
 }
 
 func checkError(err error) {
-    if err != nil {
-        fmt.Println("error occur", err)
-        os.Exit(1)
-    }
+	if err != nil {
+		fmt.Println("error occur", err)
+		os.Exit(1)
+	}
 }
--- src/websocket/main.go
+++ src/websocket/main.go
@@ -1,8 +1,40 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
 
-// go get golang.org/x/net/websocket 
+	"golang.org/x/net/websocket"
+)
+
+func Echo(ws *websocket.Conn) {
+	var err error
+	for {
+		var reply string
+
+		// &reply 是一个指针地址
+		if err = websocket.Message.Receive(ws, &reply); err != nil {
+			fmt.Println("Can't reply")
+			break
+		}
+
+		fmt.Println("Receive message from client: ", reply)
+		msg := "Receive: " + reply
+
+		if err = websocket.Message.Send(ws, msg); err != nil {
+			fmt.Println("Can't send")
+			break
+		}
+	}
+}
+
+// go get golang.org/x/net/websocket
 // 下载websocket包
 func main() {
-    
+	http.Handle("/", websocket.Handler(Echo))
+
+	if err := http.ListenAndServe(":38080", nil); err != nil {
+		log.Fatal("Listen port 38080 error", err)
+	}
 }
