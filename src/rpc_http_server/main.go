9af70bf5a4527f9753a0a10640ae813ce4cc8495
ooobenooo <benhuang81@gmail.com>
2019-07-30 09:33:15 +0000

    rpc http server

---
+++ src/rpc_http_server/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+    "error"
+    "fmt"
+    "net/http"
+    "net/rpc"
+)
+
+type Args struct {
+    A, B int
+}
+
+type Quotient struct {
+    Quo， Rem
+}
+
+type Arith int
+
+func main() {
+    
+}
